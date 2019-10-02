ffd8c17a6b60e6245e6ee37e0a979e20e838c6e7
danilmunoz1996 <danilmunoz@udec.cl>
2019-10-02 19:37:14 -0300

    template home integrado

--- info_move/views.py
+++ info_move/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
 	return render(request, 'index.html')
+
+def ListaConductores(request):
+	return render(request, 'Lista.html')
