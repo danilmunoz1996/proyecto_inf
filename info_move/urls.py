c612d7eb9be624de180fa84126eb8c22a9d9a26d
danilmunoz1996 <danilmunoz@udec.cl>
2019-10-03 23:14:49 -0300

    busqueda de lineas

--- info_move/urls.py
+++ info_move/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('ListaMicros', ListaMicros, name = 'ListaMicros'),
     path('ComentariosChofer', ComentariosChofer, name = 'ComentariosChofer'),
     path('valorar', CrearValoracion, name = 'CrearValoracion'),
+    path('busqueda_linea', SearchResultsView.as_view(), name = 'buscar_linea'),
     url(r'^usuario/(?P<pk>\d+)/$', VerPerfilUsuario, name='VerPerfilUsuario'),
     url('conductor/(?P<pk>\d+)', VerPerfilConductor, name='VerPerfilConductor'),
     path('debugger',debugger,name='debugger'),
